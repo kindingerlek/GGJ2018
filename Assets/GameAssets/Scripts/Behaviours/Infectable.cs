f1a4b23ca42d1aef55a6c1b3f85d60ce377e1020
Joao Vitor P. Moraes <jvlppm@gmail.com>
2018-01-27 14:31:02 -0200

    NavMesh and walking infected.

--- Assets/GameAssets/Scripts/Behaviours/Infectable.cs
+++ Assets/GameAssets/Scripts/Behaviours/Infectable.cs
@@ -1,20 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using MonsterLove.StateMachine;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(Renderer))]
+[RequireComponent(typeof(NavMeshAgent))]
 public class Infectable : MonoBehaviour {
+
+    public enum States
+    {
+        Init,
+        Walking
+    }
 
     public GameObject infectedBy = null;
 
+    [SerializeField] Walking walking;
+
+    NavMeshAgent agent;
     private new Renderer renderer;
     private Material originalMaterial;
 
+    StateMachine<States> fsm;
 
     public void Awake()
     {
         renderer = GetComponent<Renderer>();
+        agent = GetComponent<NavMeshAgent>();
 
         originalMaterial = renderer.material;
+
+        InitStates();
+    }
+
+    void InitStates()
+    {
+        fsm = StateMachine<States>.Initialize(this);
+        fsm.ChangeState(States.Walking);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        GetState().OnCollisionEnter(other);
     }
 
     public void Infect(GameObject infector)
@@ -36,6 +65,61 @@ public class Infectable : MonoBehaviour {
         infectedBy = infector;
 
         renderer.material = infector.GetComponent<Renderer>().material;
-
     }
+
+    #region States
+    IStateHandler GetState()
+    {
+        switch (fsm.State) {
+            case States.Walking: return walking;
+        }
+        throw new ArgumentException();
+    }
+
+    public IEnumerator Walking_Enter()
+    {
+        while (fsm.State == States.Walking)
+            yield return walking.WalkToRandomPosition(agent);
+    }
+    #endregion
+
+    #region States Data
+    interface IStateHandler {
+        void OnCollisionEnter(Collision other);
+    }
+
+    [Serializable]
+    struct Walking : IStateHandler
+    {
+        [SerializeField] float randomMovementRange;
+        [SerializeField] float maxWaitBeforeWalk;
+
+        bool didCollide;
+
+        public IEnumerator WalkToRandomPosition(NavMeshAgent agent)
+        {
+            didCollide = false;
+            var nextPos = GetRandomWalkPoint(agent.transform);
+            agent.destination = nextPos;
+
+            yield return new WaitForSeconds(UnityEngine.Random.value * maxWaitBeforeWalk);
+
+            while (!didCollide && (agent.pathPending || agent.remainingDistance > agent.stoppingDistance)) {
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+
+        void IStateHandler.OnCollisionEnter(Collision other)
+        {
+            didCollide = true;
+        }
+
+        Vector3 GetRandomWalkPoint(Transform transform)
+        {
+            var randomPosition = transform.position + UnityEngine.Random.insideUnitSphere * randomMovementRange;
+            randomPosition.y = transform.position.y;
+            return randomPosition;
+        }
+    }
+    #endregion
 }
