559f8e77c6d2f0fcc462e333d41dedfddafbd544
kindingerlek <kindingerlek@hotmail.com>
2018-01-27 03:49:34 -0200

    fix worng player movement

--- Assets/GameAssets/Scripts/Behaviours/Player.cs
+++ Assets/GameAssets/Scripts/Behaviours/Player.cs
@@ -6,14 +6,13 @@ public class Player : MonoBehaviour {
     [HideInInspector]
     public int playerIndex;
 
-    [SerializeField] private float speed = 2f;
+    [SerializeField] private float speed = 10;
     [SerializeField] public float maxVelocityChange = 10.0f;
     [SerializeField] public bool canJump = true;
     [SerializeField] public float jumpHeight = 2.0f;
 
 
-
-    private Vector3 moveDirection;
+    
     private PlayerInput input;
     private new Rigidbody rigidbody;
 
@@ -24,13 +23,13 @@ public class Player : MonoBehaviour {
         rigidbody = this.GetComponent<Rigidbody>();
 
         rigidbody.freezeRotation = true;
-        rigidbody.useGravity = false;
     }
 
     void FixedUpdate()
     {
         // Calculate how fast we should be moving
         Vector3 targetVelocity = input.worldAxisDirectionClamped;
+        targetVelocity = transform.TransformDirection(targetVelocity);
         targetVelocity *= speed;
 
         // Apply a force that attempts to reach our target velocity
