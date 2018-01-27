f6419403a18878153170dd6426f65698f92feae7
alissonkrul <alisson.krul@tuntscorp.com>
2018-01-27 11:59:36 -0200

    feat(RuleManager)

---
+++ Assets/GameAssets/Scripts/Managers/RuleManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class RuleManager : MonoBehaviour
+{
+
+
+    public enum infectationsTypes { red, blue, green, brown };
+
+
+    int[] diseasesRule = { (int)infectationsTypes.red, (int)infectationsTypes.blue, (int)infectationsTypes.green, (int)infectationsTypes.brown };
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void NextRule()
+    {
+        diseasesRule.Shuffle();
+    }
+
+
+   
+    public int CompareInfectation(Infectable infect1, Infectable infect2)
+    {
+
+        int d1 = infect1.GetInfectationType();
+        int d2 = infect2.GetInfectationType();
+        int d1Pos = 0;
+        int d2Pos = 0;
+
+        for (int i = 0; i < diseasesRule.Length; i++) {
+            if (diseasesRule[i] == d1) {
+                d1Pos = i;
+            }
+        
+            if (diseasesRule[i] == d2)
+            {
+                d2Pos = i;
+            }
+        }
+
+        if (d1Pos +1 == d2Pos|| (d1Pos==3 && d2Pos==0)) {
+            return 1;
+        }
+
+        if (d2Pos + 1 == d1Pos || (d2Pos == 3 && d1Pos == 0))
+        {
+            return -1;
+        }
+
+        return 0;
+    }
+}
