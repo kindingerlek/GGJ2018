e93b6862b08bf177e41872c2c2a0e93bed3ac6bd
maaryhabad <maryh.abad@gmail.com>
2018-01-27 15:58:28 -0200

    audio manager

---
+++ Assets/GameAssets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : SingletonMonoBehaviour<AudioManager> {
+
+	static AudioManager()
+    {
+        Lazy = false;
+        FindInactive = true;
+        DestroyOthers = DestroyOptions.DestroyGameObject;
+        Persist = true;
+    }
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
